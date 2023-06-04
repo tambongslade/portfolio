9688209ea6153ec81b77b707d75f12c65b6b0ce3
Your Name <you@example.com>
2023-06-04 18:23:23 +0100

    swiperjs added

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,10 +1,86 @@
 import React from 'react'
 import './testimonial.css'
-export const Testimonial = () => {
+import T1 from '../../assets/daddy.jpg'
+import T2 from '../../assets/mom.jpg'
+import T3 from '../../assets/Mbone.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+    {
+      id: 1,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T1,
+      name: "Mbo Valentine",
+    },
+    {
+      id: 2,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T2,
+      name: "Mbah Elsie",
+    },
+    {
+      id: 3,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T3,
+      name: "Arrah Mbone",
+    },
+    {
+      id: 4,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus est, consequatur illum ullam numquam voluptate necessitatibus sequi quidem obcaecati! Veritatis repellendus provident neque. Porro, id illum iure soluta vel alias, maiores tempora doloremque vitae neque maxime at, facilis ipsum molestiae dicta quasi harum optio atque deserunt quis amet consectetur perspiciatis.",
+      avatar: T2,
+      name: "Njang Elsie",
+
+    },
+  ];
+
+const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section id='testimonials'>
+      <h5>review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+       // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+          return(
+         
+        <SwiperSlide  key={index} className="testimonial">
+          <div className="client__avatar">
+            <img src={avatar}/>
+         </div>
+            <h5 className='client__name'>{name}</h5>
+            <small className='client__review'>
+            {review}
+            </small>
+          
+        </SwiperSlide>
+          )
+           })
+         
+         }
+        
+      </Swiper>
+    </section>
   )
 }
 
-;
-export default Testimonial;
+export default Testimonial
